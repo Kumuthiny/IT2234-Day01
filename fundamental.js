7168133d7916c45eeb304df94ddbefaafff5ec5f
Kumuthiny <kumuthiny0726@gmail.com>
2025-03-14 12:13:01 +0530

    Add files via upload

---
+++ fundamental.js
@@ -0,0 +1,14 @@
+console.log("hello");
+
+var x=10;
+var y=20;
+
+var add = x+y;
+var sub = y-x;
+var mult = x*y;
+var div = y/x;
+
+console.log(add);
+console.log(sub);
+console.log(mult);
+console.log(div);
---
+++ reverse2.js
@@ -0,0 +1,24 @@
+//reverse the numbers in left and right 4321-5-9876
+//find a middle number and reverse before and after
+
+let mid =5
+
+let max = 9
+
+for(let i=mid; i>=1; i--)
+{
+	if(i<mid)
+	{
+		console.log(i);
+	}
+	
+}
+console.log(mid)
+for(let i=max; i>=mid; i--)
+{
+	if(i>mid)
+	{
+		console.log(i);
+	}
+	
+}
